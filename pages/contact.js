87336adf2ab5b4c5273331254171ff216632d29c
Zubair Ahmed <zubair.ahmed@amperity.com>
2024-01-28 22:53:13 -0800

    about and contact

---
+++ pages/contact.js
@@ -0,0 +1,54 @@
+import Head from "next/head";
+import PageHeader from "@components/PageHeader";
+import { Global, css } from "@emotion/react";
+
+// idk wtf this actually means. But this is how i set the outermost div or body? to be the width of the screen without any scrolling
+const GlobalStyle = css`
+  body {
+    width: 100vw
+    margin: 0;
+    padding: 0;
+    background-color: black;
+  }
+`;
+
+const maindiv = css({
+  overflowX: "hidden",
+  backgroundColor: "black",
+  columnCount: 3,
+});
+
+const column = css({
+  display: "flex",
+  flexDirection: "column",
+  padding: "20%",
+  paddingTop: 0,
+  paddingBottom: 0,
+  color: "white"
+});
+
+const vid = css({
+  paddingTop: 10,
+});
+
+
+export default function Contact() {
+  return (
+    <div //css={maindiv}
+      className="container"
+    >
+      <Head>
+        <title>Contact</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Global styles={GlobalStyle} />
+
+      <PageHeader />
+
+      <div css={column}>
+       email: zugood.lasers@gmail.com
+       I will probably need to add a form to send me messages
+      </div>
+    </div>
+  );
+}
